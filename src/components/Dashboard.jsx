a9a7ef87f43512853468f13b9fdb01d0fd038787
Ваше Ім'я <_email@example.com>
2025-07-18 07:15:16 +0200

    Add node_modules to .gitignore

--- src/components/Dashboard.jsx
+++
@@ -1,9 +0,0 @@
-export default function Dashboard() {
-  return (
-    <div className="dashboard-container">
-      <h1 style={{ color: 'white', textAlign: 'center', marginTop: '100px' }}>
-        ✅ Welcome to Dashboard
-      </h1>
-    </div>
-  );
-}
